b3a3bd8561f0b25a82190445679547e148451460
Taylor Cox <taylorashtoncox@gmail.com>
2017-08-14 14:14:24 -0400

    Completed all exercises

--- main.js
+++ main.js
@@ -7,7 +7,17 @@
 //            /____/
 
 // Dog Constructor & Prototype
-function Dog () {}
+function Dog(name, status, color, hungry, owner) {
+  this.name = name
+  this.status = status
+  this.color = color
+  this.hungry = hungry
+  this.owner = owner
+}
+
+let sadie = new Dog('sadie', 'normal', 'black', false, 'Mason')
+let moonshine = new Dog('moonshine', 'normal', 'brown', true)
+let atticus = new Dog('atticus')
 
 // Instances of Dog
 // Needed: sadie, moonshine, atticus
@@ -19,7 +29,27 @@ function Dog () {}
 // /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
 
 // Human Constructor & Prototype
-function Human () {}
+function Human(name, pet, cool, feed) {
+  this.name = name
+  this.pet = function(Dog) {
+    if (Dog.name = this.pet) {
+      Dog.status = 'happy'
+    } else {
+      Dog.status = 'normal'
+    }
+  }
+  this.feed = function(Dog) {
+    if (Dog.name = this.pet) {
+      Dog.hungry = false
+    } else {
+      Dog.hungry = true
+    }
+  }
+  this.cool = cool
+}
+
+let mason = new Human('mason', 'sadie', false)
+let julia = new Human('julia', '', true)
 
 // Instances of Human
 // Needed: mason, julia
